d36bb5a533b0c771eaf308e34ad8c2d7ca7347b5
Sneider <electronsgm@gmail.com>
2022-12-30 10:39:49 -0500

    FEAT(/react): Add the useQuery hook for to get data

--- react/components/DepartmentSearch.tsx
+++ react/components/DepartmentSearch.tsx
@@ -1,8 +1,16 @@
+/* eslint-disable no-console */
 /* eslint-disable prettier/prettier */
 import React from 'react'
+import { useQuery } from 'react-apollo'
+
+import QUERY_VALUE from '../graphql/getDepartmentSearch.graphql'
 
 const DepartmentSearch = () => {
-  return <div>Search Department</div>
+  const { data, loading } = useQuery(QUERY_VALUE)
+
+  console.log('Query Data: ', data)
+
+  return loading ? <div>Loading...</div> : <div>Search Department</div>
 }
 
 export default DepartmentSearch
