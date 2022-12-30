ca9053b25465e39be5cb78c7e2e1be3688b2ada7
Sneider <electronsgm@gmail.com>
2022-12-29 21:44:20 -0500

    FEAT(/react): Add a SearchDepartment react component

---
+++ react/DepartmentSearch.tsx
@@ -0,0 +1,4 @@
+/* eslint-disable prettier/prettier */
+import DepartmentSearch from './components/DepartmentSearch'
+
+export default DepartmentSearch
---
+++ react/components/DepartmentSearch.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+import React from 'react'
+
+const DepartmentSearch = () => {
+  return <div>Search Department</div>
+}
+
+export default DepartmentSearch
